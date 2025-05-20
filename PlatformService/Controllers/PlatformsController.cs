152495e53d5089e286a3f5b74dc20e77a4240f14
Stimm <timothyryan.dublin@gmail.com>
2025-05-20 08:16:29 +0100

    Seting up models, Dto objects and controlers.

---
+++ PlatformService/Controllers/PlatformsController.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+using PlatformService.Models;
+
+namespace PlatformService.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class PlatformsController : ControllerBase
+{
+    private readonly IPlatformRepo _repository;
+    private readonly IMapper _mapper;
+
+    public PlatformsController(IPlatformRepo repository, IMapper mapper)
+    {
+        _repository = repository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+    {
+        var results = _repository.GetAllPlatforms();
+
+        return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(results));
+    }
+
+    [HttpGet("{id}", Name = "GetPlatfromById")]
+    public ActionResult<PlatformReadDto> GetPlatfromById(int id)
+    {
+        var result = _repository.GetPlatformById(id);
+
+        if(result == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(_mapper.Map<PlatformReadDto>(result));
+    }
+
+    [HttpPost]
+    public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreateDto request)
+    {
+        var platformModel = _mapper.Map<Platform>(request);
+        _repository.CreatePlatform(platformModel);
+        _repository.SaveChanges();
+
+        var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+        return CreatedAtRoute(nameof(GetPlatfromById), new { id = platformReadDto.Id }, platformReadDto);
+    }
+}
--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -21,8 +21,8 @@ public static class PrepDb
 
             context.Platforms.AddRange(
                 new Platform() { Name = "Dot Net", Id = 1, Publisher = "Microsoft", Cost="Free"},
-                new Platform() { Name = "SQL Server Express", Id = 1, Publisher = "Microsoft", Cost = "Free" },
-                new Platform() { Name = "Dot Kuberneties", Id = 1, Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
+                new Platform() { Name = "SQL Server Express", Id = 2, Publisher = "Microsoft", Cost = "Free" },
+                new Platform() { Name = "Dot Kuberneties", Id = 3, Publisher = "Cloud Native Computing Foundation", Cost = "Free" }
             );
             
             context.SaveChanges();
